354bf1c5ca06ab380998e817f4e4531ac9be0467
MulletBoy42 <96805001+MulletBoy42@users.noreply.github.com>
2022-04-11 23:55:45 -0500

    string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-
-  selector: 'app-server',
-  templateUrl: './servers.component.html',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers',
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
