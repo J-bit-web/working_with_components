2942c7f5708db8d50936132da803787d800be7c6
MulletBoy42 <96805001+MulletBoy42@users.noreply.github.com>
2022-04-21 05:32:57 -0500

    two-way data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  // selector: '[app-servers]',
-  // selector: '.app-servers',
+imrt { Component, OnInit } from '@angular/core'
+@Cponent({
+   selector: '[app-servers]',
+   selector: '.app-servers',
   selector: 'app-servers',
-  //template: `
-  //<app-server></app-server>
-  // <app-server></app-server>`,
+  template: `
+  <app-server></app-server>
+   <app-server></app-server>`,
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
 
   constructor() {
     setTimeout(() => {
@@ -26,6 +26,10 @@ export class ServersComponent implements OnInit {
   onCreateServer() {
     this.serverCreationStatus = 'Server was created!';
 
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement> event.target).value;
   }
 
 }
